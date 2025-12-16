c803a856c0e8b9f4e3315dff62237d7dff2d7a1a
sharf <57587340+esharf@users.noreply.github.com>
2025-12-16 11:08:01 +0200

    en<->he 0.0.7

--- Formula/en-he-replace.rb
+++ Formula/en-he-replace.rb
@@ -1,20 +1,20 @@
 class EnHeReplace < Formula
   desc "Replace English text with Hebrew equivalents on macOS"
   homepage "https://github.com/esharf/en-he-replace"
-  url "https://github.com/esharf/en-he-replace/releases/download/v0.0.6/en-he-replace-macos.zip"
-  sha256 "64111d9ef29c5479eb9f073c493f6bb80017e5fd1a939f54ff4902a58f695460"
-  version "0.0.6"
+  url "https://github.com/esharf/en-he-replace/releases/download/v0.0.7/en-he-replace-macos.zip"
+  sha256 "228015d31299083b4633f3f97ce1ded0e96c189993ca40f3d0a69901e20e3057"
+  version "0.0.7"
 
   def install
     bin.install "en-he-replace"
-    pkgshare.install "en<->he.workflow"
+    pkgshare.install "resources/en<->he.workflow"
   end
 
   def caveats
     <<~EOS
       To enable the included Automator service, copy the workflow to your Services folder:
 
-        cp -R "#{pkgshare}/en<->he.workflow" "~/Library/Services/"
+        cp -R "#{pkgshare}/resources/en<->he.workflow" "~/Library/Services/"
 
       After copying, the service will be available in the Services menu.
     EOS
