f31a07e1057d217b9f0906c521391eeb29421071
sharf <57587340+esharf@users.noreply.github.com>
2025-12-18 10:32:13 +0200

    en<->he 0.0.11

--- Formula/en-he-replace.rb
+++ Formula/en-he-replace.rb
@@ -1,21 +1,25 @@
 class EnHeReplace < Formula
   desc "Replace English text with Hebrew equivalents on macOS"
   homepage "https://github.com/esharf/en-he-replace"
-  url "https://github.com/esharf/en-he-replace/releases/download/v0.0.10/en-he-replace-macos.zip"
-  sha256 "fd23001efa14ba40e7892953b20f4f3c110abf192f0f334df0cbf832b395087b"
-  version "0.0.10"
+  url "https://github.com/esharf/en-he-replace/releases/download/v0.0.11/en-he-replace-macos.zip"
+  sha256 "b85998a4c211a8a434ff1dea6a6966c655dff37919bf052ce52f65459a0f16f9"
+  version "0.0.11"
 
   def install
     bin.install "en-he-replace"
     libexec.install "resources"
   end
 
-  def post_install
-    services_dir = File.expand_path("~/Library/Services")
-    mkdir_p services_dir unless Dir.exist?(services_dir)
-    cp_r libexec/"resources/en<->he.workflow", services_dir
-  end
+def post_install
+  user_home = ENV["HOME"]
+  services_dir = File.join(user_home, "Library/Services")
+  workflow_src = libexec/"resources/en<->he.workflow"
+  workflow_dst = File.join(services_dir, "en<->he.workflow")
 
+  mkdir_p services_dir
+  rm_rf workflow_dst
+  cp_r workflow_src, workflow_dst
+end
   def caveats
     <<~EOS
       To uninstall completely, run:
