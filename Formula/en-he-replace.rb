e668c1115505d6c098119aca726ed0175acbb01b
sharf <57587340+esharf@users.noreply.github.com>
2025-12-25 10:26:04 +0200

    en<->he 0.0.12

--- Formula/en-he-replace.rb
+++ Formula/en-he-replace.rb
@@ -1,27 +1,21 @@
 class EnHeReplace < Formula
   desc "Replace English text with Hebrew equivalents on macOS"
   homepage "https://github.com/esharf/en-he-replace"
-  url "https://github.com/esharf/en-he-replace/releases/download/v0.0.11/en-he-replace-macos.zip"
-  sha256 "b85998a4c211a8a434ff1dea6a6966c655dff37919bf052ce52f65459a0f16f9"
-  version "0.0.11"
+  url "https://github.com/esharf/en-he-replace/releases/download/v0.0.12/en-he-replace-macos.zip"
+  sha256 "979ff608c6307fdc6d918a432340706d2106ae683588a19cbd6acfb8eb2a8892"
+  version "0.0.12"
 
   def install
     bin.install "en-he-replace"
-    libexec.install "resources"
+    pkgshare.install "resources"
   end
 
-def post_install
-  user_home = ENV["HOME"]
-  services_dir = File.join(user_home, "Library/Services")
-  workflow_src = libexec/"resources/en<->he.workflow"
-  workflow_dst = File.join(services_dir, "en<->he.workflow")
-
-  mkdir_p services_dir
-  rm_rf workflow_dst
-  cp_r workflow_src, workflow_dst
-end
   def caveats
     <<~EOS
+      This formula installs the en-he-replace macOS Service.
+      To enable it, copy the workflow to your Services folder:
+        cp -R #{pkgshare}/en-he-replace.workflow ~/Library/Services/
+
       To uninstall completely, run:
 
         rm -rf ~/Library/Services/en\\<-\\>he.workflow
